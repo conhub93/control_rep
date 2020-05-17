26a162a63be7f07926de80f29a575ff076104749
conhub93 <51298718+conhub93@users.noreply.github.com>
2020-05-17 18:55:17 +0100

    Update master_server.pp

--- site/role/manifests/master_server.pp
+++ site/role/manifests/master_server.pp
@@ -1,10 +1,4 @@
 class role::master_server {
   include profile::base
   include profile::agent_nodes
-  ssh_authorized_key { 'web@web_server.vm':
-    ensure => present,
-    user   => 'root',
-    type   => 'ssh-rsa',
-    key    => 'AAAAB3Nza[...]qXfdaQ==',
-  }
 }
