05b451c00d900c3366f282828abf2ca6954d0803
conhub93 <51298718+conhub93@users.noreply.github.com>
2020-05-17 19:04:53 +0100

    Update master_server.pp

--- site/role/manifests/master_server.pp
+++ site/role/manifests/master_server.pp
@@ -1,14 +1,6 @@
 class role::master_server {
   include profile::base
   include profile::agent_nodes
-  class ssh {
-    # Declare:
-    @@sshkey { $::hostname:
-      type => dsa,
-      key  => $::sshdsakey,
-    }
-    # Collect:
-    Sshkey <<| |>>
-  }
+  include profile::ssh
 
 }
