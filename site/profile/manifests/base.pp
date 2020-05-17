d26d9ef4f488eeb66cf7eda7641d22a8217adeda
conhub93 <51298718+conhub93@users.noreply.github.com>
2020-05-17 18:54:45 +0100

    Update base.pp

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -2,6 +2,12 @@ class profile::base {
   ssh_keygen { 'root': 
     home => '/root'
   }
+  @@sshkey { $::hostname:
+    type => dsa,
+    key  => $::sshdsakey,
+  }
+  Sshkey <<| |>>
+  
   user {'admin':
     ensure => present,
   }
