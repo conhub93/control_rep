d7a3efddeffbd063f34d50acbd1bccf11aa98a60
conhub93 <51298718+conhub93@users.noreply.github.com>
2020-05-17 13:16:57 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,10 @@
 node default {
   file { '/root/README.md':
      ensure => file,
-     owner => 'user_does_not_exist',
+     owner => 'nodes_do_not_exist',
      content => "I want this to throw an error so I know if node master.puppet.vm does not exist",
    }
 }
-node 'master.puppet.vmm' {
+node 'master.puppet.vm' {
   include role::master_server
 }
