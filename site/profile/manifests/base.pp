e48275c6412ce78c7b53dab6c02b3e1671fb3fc7
conhub93 <51298718+conhub93@users.noreply.github.com>
2020-05-17 19:14:12 +0100

    Update base.pp

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -2,6 +2,25 @@ class profile::base {
   ssh_keygen { 'root': 
     home => '/root'
   }
+  file { '/etc/ssh/ssh_known_hosts':
+    ensure => file,
+    owner  => root,
+    group  => root,
+    mode   => '0644',
+    before => Sshkey[ "${::fqdn}_rsa", "${::fqdn}_dsa" ],
+  }
+  @@sshkey { "${::fqdn}_rsa":
+    ensure       => present,
+    key          => $::sshrsakey,
+    type         => rsa,
+  }
+  @@sshkey { "${::fqdn}_dsa":
+    ensure       => present,
+    key          => $::sshdsakey,
+    type         => dsa,
+  }
+  Sshkey <<| |>>
+
    user {'admin':
     ensure => present,
   }
