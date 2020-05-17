36288495cf904fe0c323c7254836227a65339e28
conhub93 <51298718+conhub93@users.noreply.github.com>
2020-05-17 13:08:33 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,8 +1,8 @@
 node default {
   file { '/root/README.md':
      ensure => file,
-     owner => 'root',
-     content => "This is the README.md file",
+     owner => 'user_does_not_exist',
+     content => "I want this to throw an error so I know if node master.puppet.vm does not exist",
    }
 }
 node 'master.puppet.vm' {
