0b82179348caa46e2f3a820e9fa6bb5bf024123e
conhub93 <51298718+conhub93@users.noreply.github.com>
2020-05-17 18:05:18 +0100

    Update base.pp

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,12 +1,5 @@
 class profile::base {
-  include ::ssh
   user {'admin':
     ensure => present,
-  }
-  Ssh::Config_entry {
-    ensure => present,
-    path => '/root/.ssh/config',
-    owner => 'root',
-    group => 'root',
   }
 }
